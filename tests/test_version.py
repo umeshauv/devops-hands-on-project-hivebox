78507878c989312b06afc3caf4fd27850d729d1b
USA <70544075+umeshauv@users.noreply.github.com>
2025-09-27 13:23:19 -0400

    Merge github workflow to test python application (#5)
    * Setup GitHub action to test python application
    
    * Updated pythonpath in workflow
    
    * Changed test code from unittest to pytest, added requirements.txt
    
    ---------
    
    Co-authored-by: Umesha <umesha@nokacreative.com>

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,10 +1,5 @@
-import unittest
+import pytest
 from version import get_version
 
-class TestStringMethods(unittest.TestCase):
-
-    def test_version(self):
-        self.assertEqual(get_version(), 'v0.0.1')
-
-if __name__ == '__main__':
-    unittest.main()
+def test_version():
+    assert  get_version() == 'v0.0.1'
